52c5de54967b2a15190658a99f6a1947f4deabf3
Shijoseph <777.shijo@gmail.com>
2022-05-23 11:43:21 -0400

    upload

--- src/conditionalstatment/CompareStringusingBoolean.java
+++ src/conditionalstatment/CompareStringusingBoolean.java
@@ -4,7 +4,21 @@ public class CompareStringusingBoolean {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		String sentance1 = "my name is java";
+		String sentance2 = "my name is java";
+		String sentance3 = "my name is mava";
+		
+		boolean isSent1EqualSent2 = sentance1.equals(sentance2);
+		boolean isSent1EqualSent3 = sentance1.equals(sentance3);
+		boolean isSent2EqualSent3 = sentance2.equals(sentance3);
+		
+		System.out.println("sentance 1 is equal to sentance 2 "+ isSent1EqualSent2 );
+		System.out.println("sentance 1 is equal to sentance 3 "+ isSent1EqualSent3 );
+		System.out.println("sentance 2 is equal to sentance 3 "+ isSent2EqualSent3 );
+		
+		
+		
 	}
 
 }
