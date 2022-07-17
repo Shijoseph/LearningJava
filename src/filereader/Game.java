23106fee504d14ce2918911fdb933441f528fa7a
Shijoseph <777.shijo@gmail.com>
2022-07-16 20:10:13 -0400

    guess game

--- src/filereader/Game.java
+++ src/filereader/Game.java
@@ -3,6 +3,7 @@ package filereader;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -13,10 +14,13 @@ public class Game {
 
 		String[] moviesnames = new String[25];
 		char enteredLetter;
-		boolean ifValueMatches;
 		String nameWithoutSpace;
 		String line;
-		int count = 0;
+		
+		int count;
+		int finalCounter = 1;
+		int w;
+		int r=0;
 		Random object = new Random();
 		Scanner sc = new Scanner(System.in);
 		try {
@@ -42,35 +46,68 @@ public class Game {
 
 		System.out.println("######  Welcome to the game  ######");
 		System.out.println("you have ten chances the guess the correct name");
-		
+
 		nameWithoutSpace = moviesnames[randomNumber].replaceAll("\\s", "");
 
 		char[] character = nameWithoutSpace.toCharArray();
 		char[] dummy = new char[nameWithoutSpace.length()];
-		
-		
+
 		for (int j = 0; j < nameWithoutSpace.length(); j++) {
-			dummy[j] = '?';
+			dummy[j] = '-';
 		}
 		System.out.println(character);
 		System.out.println(dummy);
 
-		for (int k = 0; k < nameWithoutSpace.length(); k++) {
-			System.out.println("enter the letter ");
-			enteredLetter = sc.next().charAt(0);
-			for (int s = 0; s < character.length; s++) {
-				if (enteredLetter == character[s]) {
-					
-					dummy[s] = character[s];
-					System.out.println(dummy);
-					
+		Game2 game = new Game2();
+		
+		for (int k = 0; k < 50; k++)
+
+		{
+			if (Arrays.equals(dummy, character)) {
+				System.out.println("you have won the game");
+				break;
+			}
+			if (finalCounter >= 10) {
+				System.out.println("you have exhausted the chances play again");
+				break;
+			}
+			w = 0;
+			count = 0;
 			
+			enteredLetter=' ';
+			
+
+			System.out.println("enter the letter ");
+
+			enteredLetter = sc.next().charAt(0);
+
+			for (int s = 0; s < character.length; s++) {
+
+				if (enteredLetter == character[s]) {
+
+					dummy[s] = character[s];
+
+					System.out.println(dummy);
+					count = 1;
+				}
+
+				else {
+					w = 1;
+				}
+
+			}
+			 
+			
+			if (w == 1 && count == 0 ) {
+				System.out.println("you have entered a wrong letter,  attempts left  " + (10 - finalCounter));
+				finalCounter++;
+				r=0;
 
 			}
 
+			
 		}
-		
-
 	}
+
 }
-}
+
